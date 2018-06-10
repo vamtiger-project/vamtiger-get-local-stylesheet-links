598a6856814583dfb7c0aee1d16e2584e56029cf
James-Alex Matthews <vamtiger@vamtiger.com>
2018-06-10 18:18:30 +0200

    vamtiger-node-typescript-commit

--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-export default function getLocalStylesheetPaths(params: Params): Promise<string[]>;
-export interface Params {
-    html: string;
-}
-export interface GetLocalStylesheetPathParams {
-    url: string;
-}
